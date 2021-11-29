3ec36a2d994e426465e764eaa466282d139c4256
Sarah Larson <slarson@mergeworld.com>
2021-11-28 22:54:01 -0500

    Create 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import { Link } from 'gatsby'
+import Icon from '../components/Icon'
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
+  <div className="u-center u-pt-30 u-pb-30">
+    <h1 className="t-h1"><span className="t-poster1">404</span><br/><span className="t-cursive">Page NOT FOUND</span></h1>
+    <p className="t-lead1">Oops! This page doesn't exist.</p>
+    <Link className="link" to="/"><Icon name="icon-arrow-left" className="icon-arrow-left icon--small u-mr-2" /> Go Home</Link>
+  </div>
 )
 
 export default NotFoundPage
